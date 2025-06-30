3b510fa8730bb7d292b0be3cccafd3962b8f7f7b
barrettlab <35808703+barrettlab@users.noreply.github.com>
2025-06-30 13:04:08 -0400

    Update Stereosandra_gbif_map.R

--- Stereosandra_gbif_map.R
+++ Stereosandra_gbif_map.R
@@ -1,3 +1,13 @@
+# Package citations:
+# rgbif: Chamberlain, S. A., et al. (2024). rgbif: Interface to the Global Biodiversity Information Facility API. R package version 3.7.7. https://CRAN.R-project.org/package=rgbif
+# dplyr: Wickham, H., et al. (2023). dplyr: A Grammar of Data Manipulation. R package version 1.1.4. https://CRAN.R-project.org/package=dplyr
+# sf: Pebesma, E. (2018). Simple Features for R: Standardized Support for Spatial Vector Data. *The R Journal*, 10(1), 439–446. https://doi.org/10.32614/RJ-2018-009
+# ggplot2: Wickham, H. (2016). *ggplot2: Elegant Graphics for Data Analysis*. Springer-Verlag New York. https://ggplot2.tidyverse.org
+# rnaturalearth: South, A. (2017). rnaturalearth: World Map Data from Natural Earth. R package version 0.1.0. https://github.com/ropensci/rnaturalearth
+# rnaturalearthdata: Natural Earth (Public domain). https://www.naturalearthdata.com/downloads/
+# ggrepel: Slowikowski, K. (2024). ggrepel: Automatically Position Non-Overlapping Text Labels with 'ggplot2'. R package version 0.9.5. https://CRAN.R-project.org/package=ggrepel
+# OpenAI. (2024). ChatGPT (April 2024 version) [Large language model]. https://chat.openai.com. Used for code debugging.
+
 # ---- 1. Load Required Packages ----
 install.packages(c("rgbif", "dplyr", "sf", "ggplot2", 
                    "rnaturalearth", "rnaturalearthdata", "ggrepel"))
